f6d33ceee2a9e49e39ffdd57432ce1fce1b5db85
emma2110 <118846214+emma2110@users.noreply.github.com>
2023-12-14 23:45:21 +0100

    Update ExactAlgorithm.h

--- src/ExactAlgorithm.h
+++ src/ExactAlgorithm.h
@@ -1,14 +1,15 @@
-//
-// Created by mpait on 21/11/2023.
-//
-
 #ifndef GRAPHTHEORYPROJECT_GROUP2_EXACTALGORITHM_H
 #define GRAPHTHEORYPROJECT_GROUP2_EXACTALGORITHM_H
 
 
-class ExactAlgorithm {
 
-};
+#include <bits/stdc++.h>
+#include "GraphAdjacencyList.h"
+using namespace std;
+
+void affiche_vector(vector <int> v);
+void powerSet(GraphAdjacencyList G, vector<int> vertices, int& min_cut, vector<int>& best, int index = -1, vector<int> curr = {});
+vector<int> deduire_subgraph2(vector<int> vertices, vector<int> subgraph1);
 
 
 #endif //GRAPHTHEORYPROJECT_GROUP2_EXACTALGORITHM_H
